80a3dd4f1293ae9462e889eeedbf488e88286beb
Hayden Ong <hayden.nimo@ymail.com>
2017-04-29 16:15:31 +0800

    updated map

--- app/map.js
+++ app/map.js
@@ -1,11 +1,31 @@
 var map;
+var marker;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 17.277,
+            lng: 121.807
         },
-        zoom: 8
+        zoom: 20,
+        mapTypeId: 'satellite'
     });
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {
+            lat: 17.277,
+            lng: 121.807
+        }
+    });
+    marker.addListener('click', toggleBounce);
+}
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
 }
